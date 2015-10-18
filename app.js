f47da15fedfeb73cd396d5e7f7505309b2111729
Jecvay <ljwjiewei@gmail.com>
2015-10-18 15:24:11 +0800

    add vscode hightligh support

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport'),
+    LocalStrategy = require('passport-local');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +23,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// passport
+app.use(express.session({ secret: 'fuwiojlk32R#@FEWgsfdsfw3io' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res) {
+  res.send("hello, please login first");
 });
 
 module.exports = router;
